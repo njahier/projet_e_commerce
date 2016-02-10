a14d4e23192564841cc1e362e471c5e62ce2e7a5
Barthes Lucie <lucieanaisbarthes@gmail.com>
2016-02-10 13:17:37 +0100

    suite

---
+++ js/catalogue.js
@@ -0,0 +1,54 @@
+
+
+
+// function afficherArticle(){
+//   var indiceArticle = urlParams.paramIndice;
+//   var article = catalogue[indiceArticle];
+//
+//   var titre = document.getElementById('titre');
+//   titre.innerHTML = article.titre;
+//   var description = document.getElementById('description');//id fiche.html
+//   description.innerHTML = article.description; //id index.html
+//   var photo = document.getElementById('image');
+//   photo.src = article.photo;
+//   console.log(urlParams);
+// }
+
+function afficherCatalogue() {
+  var elementCatalogue = document.getElementById('catalogue');
+
+  for (var i = 0; i < catalogue.length; i++) {
+    var elementArticle = document.createElement("div");
+    elementArticle.className = "article";
+    elementCatalogue.appendChild(elementArticle);
+
+    var elementRow = document.createElement("div");
+    elementRow.className = "row";
+    elementArticle.appendChild(elementRow);
+
+    var elementcolMd = document.createElement("div");
+    elementcolMd.className = "col-md-6";
+    elementRow.appendChild(elementcolMd);
+
+    var elementImg = document.createElement("img");
+    elementImg.src = catalogue[i].photo;
+    elementcolMd.appendChild(elementImg);
+
+    var elementcolMd = document.createElement("div");
+    elementcolMd.className = "col-md-6";
+    elementRow.appendChild(elementcolMd);
+
+    var elementTitre = document.createElement("h3");
+    elementTitre.innerHTML = catalogue[i].titre;
+    elementRow.appendChild(elementTitre);
+
+    for (var j = 0; j < 5 ; j++) {
+      var elementNote = document.createElement("i");
+      elementNote.className = "fa fa-reddit-alien";
+      elementRow.appendChild(elementNote);
+    }
+    var elementParagraphe = document.createElement("p");
+    elementParagraphe.innerHTML = catalogue[i].description;
+    elementRow.appendChild(elementParagraphe);
+  }
+}
---
+++ js/fake_bdd.js
@@ -0,0 +1,57 @@
+var article1={
+  photo : "http://placehold.it/100x100",
+  description : "tjdjldfjldjf",
+  titre : "t-shirt garçon",
+}
+var article2={
+  photo : "http://placehold.it/150x100",
+  description : "kdfhqhsudejldjf",
+  titre : "t-shirt fille",
+}
+var article2={
+  photo : "http://placehold.it/150x100",
+  description : "kdfhqhsudejldjf",
+  titre : "t-shirt fille",
+}
+var article2={
+  photo : "http://placehold.it/150x100",
+  description : "kdfhqhsudejldjf",
+  titre : "t-shirt fille",
+}
+var article2={
+  photo : "http://placehold.it/150x100",
+  description : "kdfhqhsudejldjf",
+  titre : "t-shirt fille",
+}
+var article2={
+  photo : "http://placehold.it/150x100",
+  description : "kdfhqhsudejldjf",
+  titre : "t-shirt fille",
+}
+var article2={
+  photo : "http://placehold.it/150x100",
+  description : "kdfhqhsudejldjf",
+  titre : "t-shirt fille",
+}
+var article2={
+  photo : "http://placehold.it/150x100",
+  description : "kdfhqhsudejldjf",
+  titre : "t-shirt fille",
+}
+var article2={
+  photo : "http://placehold.it/150x100",
+  description : "kdfhqhsudejldjf",
+  titre : "t-shirt fille",
+}
+var article2={
+  photo : "http://placehold.it/150x100",
+  description : "kdfhqhsudejldjf",
+  titre : "t-shirt fille",
+}
+var article2={
+  photo : "http://placehold.it/150x100",
+  description : "kdfhqhsudejldjf",
+  titre : "t-shirt fille",
+}
+
+var catalogue = [article1, article2, article3,article4,article5,article6,article7,article8,article9,article10];
